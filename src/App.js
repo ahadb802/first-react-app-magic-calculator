e2cbaf467e455d496e6b14b535ae2a180f7d8126
ahadb802 <abdulahadb802@gmail.com>
2023-01-11 02:13:28 +0500

    add class base app js file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Calculator from './components/Calculator';
+
+class App extends React.PureComponent {
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App;
