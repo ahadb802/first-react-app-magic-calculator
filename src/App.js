6c79c1506d20aa71ef9c9215c03d66a2cb39c808
ahadb802 <abdulahadb802@gmail.com>
2023-01-12 16:06:05 +0500

    added required components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import Calculator from './components/Calculator';
+import Calculator from './components/calculator/Calculator';
 
 class App extends React.PureComponent {
   render() {
