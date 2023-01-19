86e97d4b633a745aa58031b837519d730b4b3e2a
ahadb802 <abdulahadb802@gmail.com>
2023-01-20 01:25:08 +0500

    added pages of home and quotes

---
+++ src/components/pages/Home/home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => (
+  <section className="home">
+    <div className="home-container">
+      <h2>Welcome To My Magic App Calculator</h2>
+      <p className='text'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Magni autem mollitia ipsam nam consectetur voluptates dolorem
+        dolorum magnam nobis, ad hic vel doloremque maiores possimus.
+        Atque, voluptatum sequi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Magni autem mollitia ipsam nam consectetur voluptates dolorem
+        dolorum magnam nobis, ad hic vel doloremque maiores possimus.
+        Atque, voluptatum sequi.  
+      </p>
+      <p className='text'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Magni autem mollitia ipsam nam consectetur voluptates dolorem
+        dolorum magnam nobis, ad hic vel doloremque maiores possimus.
+        Atque, voluptatum sequi. 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Magni autem mollitia ipsam nam consectetur voluptates dolorem
+        dolorum magnam nobis, ad hic vel doloremque maiores possimus.
+        Atque, voluptatum sequi. 
+      </p>
+    </div>
+  </section>
+);
+
+export default Home;
---
+++ src/components/pages/Quotes/quotes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './quotes.css';
+
+const Quote = () => (
+  <section className="quotes-container">
+    <div className="quotes">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Magni autem mollitia ipsam nam consectetur voluptates dolorem
+        dolorum magnam nobis, ad hic vel doloremque maiores possimus.
+        Atque, voluptatum sequi
+      </p>
+    </div>
+  </section>
+);
+
+export default Quote;
