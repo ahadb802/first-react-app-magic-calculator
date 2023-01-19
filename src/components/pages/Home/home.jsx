3ed4d8ec40b9f574e88655e3cb16e7c0a72e9408
ahadb802 <abdulahadb802@gmail.com>
2023-01-20 02:37:04 +0500

    fix errors

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -5,7 +5,7 @@ const Home = () => (
   <section className="home">
     <div className="home-container">
       <h2>Welcome To My Magic App Calculator</h2>
-      <p className='text'>
+      <p className="text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Magni autem mollitia ipsam nam consectetur voluptates dolorem
         dolorum magnam nobis, ad hic vel doloremque maiores possimus.
@@ -13,17 +13,17 @@ const Home = () => (
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Magni autem mollitia ipsam nam consectetur voluptates dolorem
         dolorum magnam nobis, ad hic vel doloremque maiores possimus.
-        Atque, voluptatum sequi.  
+        Atque, voluptatum sequi.
       </p>
-      <p className='text'>
+      <p className="text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Magni autem mollitia ipsam nam consectetur voluptates dolorem
         dolorum magnam nobis, ad hic vel doloremque maiores possimus.
-        Atque, voluptatum sequi. 
+        Atque, voluptatum sequi.
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Magni autem mollitia ipsam nam consectetur voluptates dolorem
         dolorum magnam nobis, ad hic vel doloremque maiores possimus.
-        Atque, voluptatum sequi. 
+        Atque, voluptatum sequi.
       </p>
     </div>
   </section>
